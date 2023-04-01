6f85313d96fa24d447716b89022ee7d7aac99149
Leigh Michael Forrest <leigh@catbirdseat.io>
2023-04-01 08:49:46 -0400

    Pytest-django added

--- app/tests/customuser_tests.py
+++ app/tests/customuser_tests.py
@@ -1,38 +1,40 @@
-from django.test import TestCase
+import pytest
 from django.contrib.auth import get_user_model
+
 
 User = get_user_model()
 
-class UsersManagersTest(TestCase):
+@pytest.mark.django_db
+class TestUsersManager:
     def test_create_user(self):
         user = User.objects.create_user(email="rod@example.com", password="testPass123")
-        self.assertEqual(user.email,"rod@example.com")
-        self.assertTrue(user.is_active)
-        self.assertFalse(user.is_staff)
-        self.assertFalse(user.is_superuser)
+        assert user.email =="rod@example.com"
+        assert user.is_active
+        assert not user.is_staff
+        assert not user.is_superuser
         
         try:
-            self.assertIsNone(user.username)
+            assert user.usrname is None
         except AttributeError:
             pass
-        with self.assertRaises(TypeError):
+        with pytest.raises(TypeError):
             User.objects.create_user()
-        with self.assertRaises(TypeError):
+        with pytest.raises(TypeError):
             User.objects.create_user(email="")
-        with self.assertRaises(ValueError):
+        with pytest.raises(ValueError):
             User.objects.create_user(email="", password="Foo")
     
     def test_create_superuser(self):
         admin_user = User.objects.create_superuser(email="clark@dailyplanet.com", password="TestPass123")
-        self.assertEqual(admin_user.email, "clark@dailyplanet.com")
-        self.assertTrue(admin_user.is_active)
-        self.assertTrue(admin_user.is_staff)
-        self.assertTrue(admin_user.is_superuser)
+        assert admin_user.email == "clark@dailyplanet.com"
+        assert admin_user.is_active
+        assert admin_user.is_staff
+        assert admin_user.is_superuser
         
         try:
-            self.assertIsNone(admin_user.username)
+            assert admin_user.username is None
         except AttributeError:
             pass
-        with self.assertRaises(ValueError):
+        with pytest.raises(ValueError):
             User.objects.create_superuser(email="super@user.com",password="Foo",
                 is_superuser=False)
